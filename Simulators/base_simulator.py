51cb736f0195cd79df765bed2588c1301d7018de
theDavideSalzano <102673449+theDavideSalzano@users.noreply.github.com>
2024-10-22 12:43:23 +0200

    Initial code

--- Simulators/base_simulator.py
+++ Simulators/base_simulator.py
@@ -1,2 +1,43 @@
 # Test commit
 
+
+class Simulator():
+
+    def __init__(self,agents,env,controller,integrator,logger,render,conf_file) -> None:
+
+        #get parameters from initialization 
+        self.agents = agents
+        self.env = env
+        self.controller = controller
+        self.logger = logger
+        self.render = render
+        self.integrator = integrator
+
+
+
+
+
+    def simulate(self):
+
+
+        for t in range(0, self.t_f, self.dt):
+
+            u = self.controller.get_action(self.agents.x,self.env.x)
+            f = self.env.get_forces(self.agents.x)
+            
+            x_new = self.integrator.step(self.agents, u, f)
+            # Uprate the environment
+
+            #Execute every N steps
+            self.logger.log(self.agents.x,u,f,self.env)
+            self.render.render(self.agents.x,u,f,self.env)
+
+
+
+            
+
+
+
+
+
+
